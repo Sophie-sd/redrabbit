f7b7caf27028e18712ea82bef91a4d7aee17162b
Sophie-sd <sofiia.dmitrenko@gmail.com>
2025-10-16 13:10:24 +0300

    Видалено директорію apps/blog

--- apps/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', views.ArticleListView.as_view(), name='list'),
-    path('<slug:slug>/', views.ArticleDetailView.as_view(), name='detail'),
-]
